857adfc358eb977d5b07f4cd5e4777ab25af9ef4
Yacine Lyoubi <yacinelyoubi37@gmail.com>
2021-03-25 23:09:40 +0100

    first commit

--- index.js
+++ index.js
@@ -1,61 +1,52 @@
-// PROMISE 
+// PROMISE
+const calculate = (a, b, c) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0 || c < 0) 
+            return reject("No numbers can be negative.");
+      resolve(a + b + c);
+    }, 1000);
+  });
+};
 
-// const calculate = (a,b,c) =>{
-//     return new Promise((resolve, reject) =>{
-//         setTimeout(() =>{
-//             if(a < 0 || b < 0 || c < 0)
-//                 return reject("No numbers can be negative.")
-//             resolve(a+b+c)
-//         }, 2000)
-//     })
-// }
 
-// calculate(1,2,3).then((addition) =>{
-//     console.log(addition);
-//     return calculate(addition, 4, 5)
-// })
-// .then((result) =>{
-//     console.log(result);
-//     return calculate(result, 6, 7)
-// })
-// .then((result2 =>{
-//     console.log(result2)
-//     return calculate(result2, 8, 9)
-// }))
-// .then((result3) =>{
-//     console.log(result3)
-// })
-// .catch(error =>{
-//     console.log(error)
-// })
-
+calculate(1, 2, 3)
+  .then((result0) => {
+    console.log(result0);
+    return calculate(result0, 4, 5);
+  })
+  .then((result1) => {
+    console.log(result1);
+    return calculate(result1, 6, 7);
+  })
+  .then((result2) => {
+    console.log(result2);
+    return calculate(result2, 8, 9);
+  })
+  .then((result3) => {
+    console.log(result3);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 /*//////////////////////////////////////////////////////////////////////////////////////////*/
 
-
 // ASYNC
 
-const calculate = (a,b,c) =>{
-    return new Promise((resolve, reject) =>{
-        setTimeout(() =>{
-            if(a < 0 || b < 0 || c < 0)
-                return reject("No numbers can be negative.")
-            resolve(a+b+c)
-        }, 2000)
-    })
-}
-
 const add = async () => {
-    const sum1 = await calculate(1,2,3);
-    const sum2 = await calculate(sum1,5,6);
-    const sum3 = await calculate(sum2,7,8);
-    const sum4 = await calculate(sum3,9,10);
-    return sum1 + sum2 + sum3 + sum4
-}
+  const sum1 = await calculate(1, 2, 3);
+  const sum2 = await calculate(sum1, 4, 5);
+  const sum3 = await calculate(sum2, 6, 7);
+  const sum4 = await calculate(sum3, 8, 9);
+  return `
+    ${sum1}  
+    ${sum2}  
+    ${sum3}   
+    ${sum4}
+            `;
+};
 
-add().then((value) => {
-    console.log(value)
-})
-.catch(error =>{
-    console.log(error)
-})
+add()
+.then((value) => console.log(value))
+.catch((error) => console.log(error))
